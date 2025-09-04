76fadf609089f048a6dbacbce603c0826582a286
Tata52 <alexcopca03@gmail.com>
2025-09-04 02:19:09 -0600

    correccion de llamados de referencia

--- index.php
+++ index.php
@@ -4,4 +4,12 @@ $page_title = "Gym Atenas Industria";
 include("../elementos/head-vcard/head.php");
 ?>
 
+<body>
+    <div class="contenedor-1">
+        <img class="logo" src="img/logo.png" alt="Gym Atenas Logo">
+        <h1>Atenas Industria Gym </h1>
+    </div>
+</body>
+
+
 <?php include("../elementos/footer-vcard/footer.html");?>
