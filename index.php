21d26a43e5522850ec1b82f89f71ee2034e1a085
Tata52 <alexcopca03@gmail.com>
2025-09-06 15:07:17 -0600

    subida de muestra a host

--- index.php
+++ index.php
@@ -5,14 +5,15 @@ include("../elementos/head-vcard/head.php");
 ?>
 
 <body>
-    <div class="contenedor-1">
-        <img class="logo" src="img/logo.png" alt="Gym Atenas Logo">
-        <div class="info-cliente">
-            <h1>ATENAS INDUSTRIA GYM</h1>
-            <h2>TU MEJOR VERSIÓN EMPIEZA AQUÍ</h2>
+    <main>
+        <div class="contenedor-1">
+            <img class="logo" src="img/logo.png" alt="Gym Atenas Logo">
+            <div class="info-cliente">
+                <h1>ATENAS INDUSTRIA GYM</h1>
+                <h2>TU MEJOR VERSIÓN EMPIEZA AQUÍ</h2>
+            </div>
         </div>
-    </div>
+        
+    </main>
+    <?php include("../elementos/footer-vcard/footer.html");?>
 </body>
-
-
-<?php include("../elementos/footer-vcard/footer.html");?>
